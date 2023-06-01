3cf8728a714e636d6784ad27c026fea19d251f6a
detailswes <details.wes@gmail.com>
2023-06-01 18:44:52 +0530

    jus checking

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(
+	require("prerender-node").set("prerenderToken",'BCk4NCFmGN2ZVreWRHMT' )
+);
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
+const port = process.env.PORT || 5121;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);    
+});
+
+module.exports = app;
