045fd5d495ece82889b16bfbafebe96c4621ffc4
detailswes <details.wes@gmail.com>
2023-06-01 18:48:09 +0530

    ch

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ const app = express();
 app.use(
 	require("prerender-node").set("prerenderToken",'BCk4NCFmGN2ZVreWRHMT' )
 );
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../","build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../","build", "index.html"));
 });
 
 
