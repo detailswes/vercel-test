6b88e2fa83bf1fab1d7532dbc32f14fd1b55af70
detailswes <details.wes@gmail.com>
2023-06-02 12:09:27 +0530

    check

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-
+console.log('run')
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
