94f0c40b00a836c56504b02b858c02f155f997b1
detailswes <details.wes@gmail.com>
2023-06-02 12:38:25 +0530

    servers up

--- servers.js
+++ servers.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
