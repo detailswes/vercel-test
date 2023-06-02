5aa3d53efb3bb9ba73e5a06d3e2ec69959b036ba
detailswes <details.wes@gmail.com>
2023-06-02 12:44:52 +0530

    update

--- servers.js
+++ servers.js
@@ -1,15 +1,23 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+const prerender = require('prerender-node');
+
 const app = express();
+
+// Use prerender.io for prerendering
+app.use(prerender.set('prerenderToken', 'BCk4NCFmGN2ZVreWRHMT'));
+
+// Serve the React app
 app.use(express.static(path.join(__dirname, 'build')));
+
+// Serve the React app for all routes
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-  // Serve the React app for all routes
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-app.listen(5000, () => {
-  console.log("Running on port 5000.");
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-// Export the Express API
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
 module.exports = app;
