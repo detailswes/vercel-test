670a709ea1864a7fb24c91f264213242c105f1e2
detailswes <details.wes@gmail.com>
2023-06-02 11:25:17 +0530

    testtest

--- server.js
+++ server.js
@@ -1,32 +1,19 @@
-const express = require('express');
-const path = require('path');
-
+const express = require("express");
+const path = require("path");
 const app = express();
-
 app.use(
-  require("prerender-node").set("prerenderToken",'BCk4NCFmGN2ZVreWRHMT' )
+	require("prerender-node").set("prerenderToken",'BCk4NCFmGN2ZVreWRHMT' )
 );
-// Serve the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-// Define your Node.js routes and logic here
-// ...
-
-
-
-
-// For all other requests, serve the React app
-app.get('*', (req, res) => {
-  // Your Node.js logic here before serving the React app
-  // ...
-
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-// Start the server
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT || 5121;
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server started on port ${port}`);    
 });
 
 module.exports = app;
